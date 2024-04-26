bfff4f1656d5d60256af16cbd181aade0e9091ad
martin <martinurb00@gmail.com>
2024-04-26 10:47:23 -0300

    first repository

---
+++ src/components/TextShine.jsx
@@ -0,0 +1,8 @@
+const TextShine = ({ text }) => {
+  return (
+    <span className="inline-flex animate-background-shine bg-[linear-gradient(110deg,#939393,45%,#1e293b,55%,#939393)] bg-[length:250%_100%] bg-clip-text text-xl text-transparent">
+      {text}
+    </span>
+  );
+};
+export default TextShine;
